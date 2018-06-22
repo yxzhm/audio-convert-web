9d6a9f048649930785a3b8552447157e1aa458e6
huiming zhang <huiming.zhang@nuance.com>
2018-06-22 14:02:20 +0800

    finish the player gui and add ws service

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-player',
@@ -6,10 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player.component.css']
 })
 export class PlayerComponent implements OnInit {
+  codecList: string[] = [];
+  paraList: string[] = [];
+  containerList: string[] = [];
+  selectedFile: File = null;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+    this.codecList.push('Speex');
+
+    this.paraList.push('NB');
+    this.paraList.push('WB');
+    this.paraList.push('UB');
+
+    this.containerList.push('None');
+    this.containerList.push('Nuance Frame');
   }
+
+  browseFile(fileInput: Event) {
+    console.log('browser file');
+    // let file = fileInput.target.files[0];
+    // let fileName = file.name;
+    this.selectedFile = (<HTMLInputElement>fileInput.target).files[0];
+  }
+
+  sendToServer() {
+    console.log('sending to server');
+  }
+
 
 }
